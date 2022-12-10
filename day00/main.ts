4b620e77a8c126c9bedd7f865da96ee495f43353
Nick <nnick1n@gmail.com>
2022-12-10 13:09:44 -0600

    add a little extra to the main.ts template

--- day00/main.ts
+++ day00/main.ts
@@ -1,5 +1,7 @@
 import fs from "fs";
 
 const input = fs.readFileSync("./day00/input.txt", "utf-8");
+// test input:
+// const input = ``;
 const lines = input.split("\n");
-const chars = input.split("");
+const chars = lines.map((line) => line.split(""));
